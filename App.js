d2aa2a20ce0323250d7630dd725e44aa51da155b
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-03-14 19:45:55 +0700

    install firebase and navigation

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{"none data"}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAnRv2cvR2S3GoBt1k2ySKJQ7dQ-cH9mco",
+    authDomain: "chatting-app-68c5b.firebaseapp.com",
+    projectId: "chatting-app-68c5b",
+    storageBucket: "chatting-app-68c5b.appspot.com",
+    messagingSenderId: "398950797880",
+    appId: "1:398950797880:web:66804efe052cfb284dcccd",
+    measurementId: "G-SQS9DZGNLY"
+  };
